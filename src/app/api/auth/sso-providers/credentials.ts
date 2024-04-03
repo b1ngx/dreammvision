50b7961f91166f961aafe6bcf7090cccb9b9dcad
Heart <b1ngx@qq.com>
2024-04-03 17:34:56 +0800

    feat: credentials

---
+++ src/app/api/auth/sso-providers/credentials.ts
@@ -0,0 +1,43 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const provider = {
+  id: 'credentials',
+  provider: CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Credentials",
+    // `credentials` is used to generate a form on the sign in page.
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    // You can pass any HTML attribute to the <input> tag through the object.
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: {  label: "Password", type: "password" }
+    },
+    async authorize(credentials) {
+      const res = await fetch('http://101.43.129.119:3000/api/user/login', {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify(credentials),
+      });
+
+      console.log(res.ok, res.status, res.statusText)
+      console.log(res)
+
+      if(!res.ok) return null
+
+      const { success, data: user } = await res.json();
+
+      if (success) {
+        // Any object returned will be saved in `user` property of the JWT
+        return { ...user, name: user.display_name };
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+    }
+  }),
+};
+
+export default provider;
--- src/app/api/auth/sso-providers/index.ts
+++ src/app/api/auth/sso-providers/index.ts
@@ -1,6 +1,7 @@
 import Auth0 from './auth0';
 import Authentik from './authentik';
 import AzureAD from './azure-ad';
+import Credentials from './credentials';
 import Github from './github';
 
-export const ssoProviders = [Auth0, Authentik, AzureAD, Github];
+export const ssoProviders = [Auth0, Authentik, AzureAD, Credentials, Github];
