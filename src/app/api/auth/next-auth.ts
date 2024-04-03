829ccc7761a63c6f9fa2561d41afe8c9b2272d28
b1ngx <b1ngx@qq.com>
2024-04-03 17:04:16 +0800

    Update next-auth.ts

--- src/app/api/auth/next-auth.ts
+++ src/app/api/auth/next-auth.ts
@@ -1,16 +1,22 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { getServerConfig } from '@/config/server';
 
-const { ENABLE_OAUTH_SSO, NEXTAUTH_SECRET } = getServerConfig();
+import { ssoProviders } from './sso-providers';
 
-declare module '@auth/core/jwt' {
-  // Returned by the `jwt` callback and `auth`, when using JWT sessions
-  interface JWT {
-    userId?: string;
-  }
-}
+const { NEXTAUTH_SECRET, ENABLE_OAUTH_SSO, SSO_PROVIDERS } = getServerConfig();
+
+export const initSSOProviders = () => {
+  return ENABLE_OAUTH_SSO
+    ? SSO_PROVIDERS.split(/[,，]/).map((provider) => {
+        const validProvider = ssoProviders.find((item) => item.id === provider);
+
+        if (validProvider) return validProvider.provider;
+
+        throw new Error(`[NextAuth] provider ${provider} is not supported`);
+      })
+    : [];
+};
 
 const nextAuth = NextAuth({
   callbacks: {
@@ -31,42 +37,7 @@ const nextAuth = NextAuth({
       return session;
     },
   },
-  providers: ENABLE_OAUTH_SSO
-    ? [
-        CredentialsProvider({
-          async authorize(credentials) {
-            // You need to provide your own logic here that takes the credentials
-            // submitted and returns either a object representing a user or value
-            // that is false/null if the credentials are invalid.
-            // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-            // You can also use the `req` object to obtain additional parameters
-            // (i.e., the request IP address)
-
-            console.log('credentials', credentials);
-
-            const res = await fetch('http://101.43.129.119:3000/api/user/login', {
-              body: JSON.stringify(credentials),
-              headers: { 'Content-Type': 'application/json' },
-              method: 'POST',
-            });
-
-            console.log(res.ok, res.status, res.statusText);
-            console.log('res', res);
-
-            if (!res.ok) return null;
-
-            const { success, data: user } = await res.json();
-
-            // If no error and we have user data, return it
-            if (success) {
-              return { ...user, name: user.display_name };
-            }
-            // Return null if user data could not be retrieved
-            return { id: '1', name: 'ai' };
-          },          
-        }),
-      ]
-    : [],
+  providers: initSSOProviders(),
   secret: NEXTAUTH_SECRET,
   trustHost: true,
 });
@@ -75,3 +46,10 @@ export const {
   handlers: { GET, POST },
   auth,
 } = nextAuth;
+
+declare module '@auth/core/jwt' {
+  // Returned by the `jwt` callback and `auth`, when using JWT sessions
+  interface JWT {
+    userId?: string;
+  }
+}
