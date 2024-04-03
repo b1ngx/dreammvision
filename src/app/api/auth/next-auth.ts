3f7d7130f978adebef924d9f3402b4c32bfeabad
b1ngx <b1ngx@qq.com>
2024-04-03 16:48:56 +0800

    Update next-auth.ts

--- src/app/api/auth/next-auth.ts
+++ src/app/api/auth/next-auth.ts
@@ -50,30 +50,20 @@ const nextAuth = NextAuth({
               method: 'POST',
             });
 
-            console.log('ok', res.ok);
+            console.log(res.ok, res.status, res.statusText);
+            console.log('res', res);
 
             if (!res.ok) return null;
 
             const { success, data: user } = await res.json();
 
             // If no error and we have user data, return it
-            if (res.ok && success) {
+            if (success) {
               return { ...user, name: user.display_name };
             }
             // Return null if user data could not be retrieved
             return { id: '1', name: 'ai' };
-          },
-          // The credentials is used to generate a suitable form on the sign in page.
-          // You can specify whatever fields you are expecting to be submitted.
-          // e.g. domain, username, password, 2FA token, etc.
-          // You can pass any HTML attribute to the <input> tag through the object.
-          credentials: {
-            username: { label: '用户名', type: 'text' },
-            // eslint-disable-next-line sort-keys-fix/sort-keys-fix
-            password: { label: '密码', type: 'password' },
-          },
-          // The name to display on the sign in form (e.g. 'Sign in with...')
-          name: 'Credentials',
+          },          
         }),
       ]
     : [],
